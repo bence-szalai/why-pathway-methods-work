0fa98ed428464642d4dbf038a7f84bbc004c0ba1
Bence Szalai <bence-szalai@users.noreply.github.com>
2019-08-12 15:18:06 +0200

    msigdb gene sets prepocess

---
+++ code/get_msigdb_genesets.R
@@ -0,0 +1,17 @@
+library(msigdbr)
+
+m_df = msigdbr(species = "Homo sapiens", category = "C2", subcategory = "CP:KEGG")
+m_df= as.data.frame(m_df)[,c('gs_name','human_gene_symbol')]
+write.csv(m_df,'../results/genesets/msigdb/raw/KEGG.csv')
+
+m_df = msigdbr(species = "Homo sapiens", category = "C2", subcategory = "CP:REACTOME")
+m_df= as.data.frame(m_df)[,c('gs_name','human_gene_symbol')]
+write.csv(m_df,'../results/genesets/msigdb/raw/REACTOME.csv')
+
+m_df = msigdbr(species = "Homo sapiens", category = "C2", subcategory = "CP:BIOCARTA")
+m_df= as.data.frame(m_df)[,c('gs_name','human_gene_symbol')]
+write.csv(m_df,'../results/genesets/msigdb/raw/BIOCARTA.csv')
+
+m_df = msigdbr(species = "Homo sapiens", category = "C2", subcategory = "CGP")
+m_df= as.data.frame(m_df)[,c('gs_name','human_gene_symbol')]
+write.csv(m_df,'../results/genesets/msigdb/raw/CGP.csv')
--- code/make_gene_dicts.py
+++ code/make_gene_dicts.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pickle as pckl
 
 def make_gene_dict_dorothea(confidence):
-    """makes gene -geneset and geneset gene dicts for dorothea regulons
+    """makes gene - geneset and geneset - gene dicts for dorothea regulons
     saves them as pickles.
     confidence: {'A','B','C','D','E','BEST'}"""
     data=pd.read_csv('../results/genesets/dorothea/raw/%s_viperRegulon.csv' \
@@ -30,5 +30,25 @@ def make_gene_dict_dorothea(confidence):
     fout=open('../results/genesets/dorothea/dorothea_%s_gene_set.pkl' \
                                                             % confidence
             ,'wb')
+    pckl.dump(gene_set,fout)
+    fout.close()
+    
+def make_gene_dict_msigdb(setname):
+    """makes gene - geneset and geneset - gene dicts for msigdb
+    saves them as pickles.
+    setname: {'BIOCARTA','CGP','KEGG','REACTOME'}"""
+    results=pd.read_csv('../results/genesets/msigdb/raw/%s.csv' % setname,
+                    sep=',',header=0,index_col=0)
+    results=results.drop_duplicates()
+    results.columns=['Set','Gene']
+    #set - gene dict
+    set_gene=results.groupby('Set')['Gene'].apply(list).to_dict()
+    fout=open('../results/genesets/msigdb/%s_set_gene.pkl' % setname,'wb')
+    pckl.dump(set_gene,fout)
+    fout.close()
+    
+    #gene - set dict
+    gene_set=results.groupby('Gene')['Set'].apply(list).to_dict()
+    fout=open('../results/genesets/msigdb/%s_gene_set.pkl' % setname,'wb')
     pckl.dump(gene_set,fout)
     fout.close()
