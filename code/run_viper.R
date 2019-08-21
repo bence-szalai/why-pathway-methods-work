11be67de8ad9436d0f93c22c41a63a92b470d499
Bence Szalai <bence-szalai@users.noreply.github.com>
2019-08-21 17:04:23 +0200

    benchmark score calculation

--- code/run_viper.R
+++ code/run_viper.R
@@ -1,11 +1,56 @@
 library(viper)
 ncores=11
 set.seed(19890904)
+#gdsc
+gdsc=read.csv('../results/benchmark/gdsc/raw/gdsc_gex.csv',sep=',',header=T,row.names = 1,check.names=FALSE)
+#msigdb
 regulon_type='msigdb'
-regulon_name='KEGG'
-load(paste0('../results/genesets/',regulon_type,'/rdata/',regulon_name,'_set_gene.rdata'))
-regulon=data
-data=read.csv('../results/benchmark/gdsc/raw/gdsc_gex.csv',sep=',',header=T,row.names = 1,check.names=FALSE)
-activities = viper(eset = data, regulon = regulon, nes = T, 
-                   method = 'none' ,minsize = 4, eset.filter = F,cores = ncores)
-write.csv()
+regulon_names=c('KEGG','BIOCARTA','REACTOME','CGP')
+
+for (regulon_name in regulon_names){
+  load(paste0('../results/genesets/',regulon_type,'/rdata/',regulon_name,'_set_gene.rdata'))
+  regulon=data
+  
+  activities = viper(eset = gdsc, regulon = regulon, nes = T, 
+                     method = 'none' ,minsize = 4, eset.filter = F,cores = ncores)
+  write.csv(activities,paste0('../results/benchmark/scores/gdsc/',regulon_name,'.csv'))
+}
+#dorothea
+regulon_type='dorothea'
+regulon_names=c('dorothea_A','dorothea_AB','dorothea_ABC','dorothea_ABCD','dorothea_ABCDE',
+                'dorothea_B','dorothea_C','dorothea_D','dorothea_E','dorothea_BEST')
+for (regulon_name in regulon_names){
+  load(paste0('../results/genesets/',regulon_type,'/rdata/',regulon_name,'_set_gene.rdata'))
+  regulon=data
+  
+  activities = viper(eset = gdsc, regulon = regulon, nes = T, 
+                     method = 'none' ,minsize = 4, eset.filter = F,cores = ncores)
+  write.csv(activities,paste0('../results/benchmark/scores/gdsc/',regulon_name,'.csv'))
+}
+
+#progeny
+progeny=read.csv('../results/benchmark/progeny/raw/progeny_data.csv',sep=',',header=T,row.names = 1,check.names=FALSE)
+#msigdb
+regulon_type='msigdb'
+regulon_names=c('KEGG','BIOCARTA','REACTOME','CGP')
+
+for (regulon_name in regulon_names){
+  load(paste0('../results/genesets/',regulon_type,'/rdata/',regulon_name,'_set_gene.rdata'))
+  regulon=data
+  
+  activities = viper(eset = progeny, regulon = regulon, nes = T, 
+                     method = 'none' ,minsize = 4, eset.filter = F,cores = ncores)
+  write.csv(activities,paste0('../results/benchmark/scores/progeny/',regulon_name,'.csv'))
+}
+#dorothea
+regulon_type='dorothea'
+regulon_names=c('dorothea_A','dorothea_AB','dorothea_ABC','dorothea_ABCD','dorothea_ABCDE',
+                'dorothea_B','dorothea_C','dorothea_D','dorothea_E','dorothea_BEST')
+for (regulon_name in regulon_names){
+  load(paste0('../results/genesets/',regulon_type,'/rdata/',regulon_name,'_set_gene.rdata'))
+  regulon=data
+  
+  activities = viper(eset = progeny, regulon = regulon, nes = T, 
+                     method = 'none' ,minsize = 4, eset.filter = F,cores = ncores)
+  write.csv(activities,paste0('../results/benchmark/scores/progeny/',regulon_name,'.csv'))
+}
