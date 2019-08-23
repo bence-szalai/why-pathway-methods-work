40f22d213dc5bf50dbe08fcac190c7fd2a4d0254
Bence Szalai <bence-szalai@users.noreply.github.com>
2019-08-23 11:41:35 +0200

    rocaucs calculated

---
+++ code/benchmark_progeny.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics import roc_auc_score as ROCAUC
+
+progeny=pd.read_csv('../results/benchmark/progeny/raw/progeny_meta.csv',
+                        sep=',',header=0,index_col=0)
+                        
+
+for method in ['KEGG','dorothea_A','dorothea_AB','dorothea_BEST',
+                'BIOCARTA','REACTOME','CGP','dorothea_ABC',
+                'dorothea_ABCD','dorothea_ABCDE','dorothea_B','dorothea_C',
+                'dorothea_D','dorothea_E']:
+    print(method)
+    scores=pd.read_csv('../results/benchmark/scores/progeny/%s.csv' % method,
+                    sep=',',header=0,index_col=0).T
+    results=pd.DataFrame(index=list(set(progeny['pathway'])),
+                            columns=scores.columns)
+    for pathway in results.index:
+        y_tr=(progeny['pathway']==pathway)*1
+        for score in results.columns:
+            y_pr=scores[score]
+            results.loc[pathway,score]=ROCAUC(y_tr,y_pr)
+    results.to_csv('../results/benchmark/progeny/rocaucs/%s.csv' % method,
+                    sep=',')
+    
