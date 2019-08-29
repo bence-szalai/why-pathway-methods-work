e1a4d2425213aeacba4ab7cf63c67b1d5786fe5b
Bence Szalai <bence-szalai@users.noreply.github.com>
2019-08-29 09:17:11 +0200

    make viper regulon nice and fast

--- code/make_viper_regulon.R
+++ code/make_viper_regulon.R
@@ -1,40 +1,19 @@
 make_regulon=function(x){
-  temp=unique(x)
-  fil=temp!='NONE'
-  temp=temp[fil]
   result=list()
-  result$tfmode=rep(1,length(temp))
-  names(result$tfmode)=temp
-  result$likelihood=rep(1,length(temp))
+  result$tfmode=rep(1,length(x))
+  names(result$tfmode)=x
+  result$likelihood=rep(1,length(x))
   result
 }
 
-fnames=c('A','B','C','D','E','AB','ABC','ABCD','ABCDE','BEST')
-
-for (fname in fnames){
-  data=read.csv(paste0('../results/genesets/dorothea/csvs/dorothea_',fname,'_set_gene.csv'),
-                sep=',',check.names=FALSE,header = T,row.names = 1)
-  data=as.matrix(data)
-  data=apply(data,1,make_regulon)
-  save(data,file=paste0('../results/genesets/dorothea/rdata/dorothea_',fname,'_set_gene.rdata'))
+dnames=c('single','random')
+for (dname in dnames){
+  fnames=list.files(paste0('../results/genesets/',dname,'/csvs/'))
+  for (fname in fnames){
+    data=read.csv(paste0('../results/genesets/',dname,'/csvs/',fname),sep = ',',header = T,row.names = 1)
+    data=split(data$Gene,data$Set)
+    viper_geneset=lapply(data,make_regulon)
+    save(viper_geneset,file=paste0('../results/genesets/',dname,'/rdatas/',substr(fname,1,nchar(fname)-4),'.rdata'))
+  }
 }
 
-fnames=c('BIOCARTA','KEGG','CGP','REACTOME')
-
-for (fname in fnames){
-  data=read.csv(paste0('../results/genesets/msigdb/csvs/',fname,'_set_gene.csv'),
-                sep=',',check.names=FALSE,header = T,row.names = 1)
-  data=as.matrix(data)
-  data=apply(data,1,make_regulon)
-  save(data,file=paste0('../results/genesets/msigdb/rdata/',fname,'_set_gene.rdata'))
-}
-
-fnames=unlist(lapply(list.files('../results/genesets/overlaps/csvs/'),FUN=function(x) {substr(x,start = 1,stop = nchar(x)-4)}))
-
-for (fname in fnames){
-  data=read.csv(paste0('../results/genesets/overlaps/csvs/',fname,'.csv'),
-                sep=',',check.names=FALSE,header = T,row.names = 1)
-  data=as.matrix(data)
-  data=apply(data,1,make_regulon)
-  save(data,file=paste0('../results/genesets/overlaps/rdata/',fname,'.rdata'))
-}
--- code/make_viper_regulon_new.R
+++
@@ -1,7 +0,0 @@
-make_regulon=function(x){
-  result=list()
-  result$tfmode=rep(1,length(x))
-  names(result$tfmode)=x
-  result$likelihood=rep(1,length(x))
-  result
-}
