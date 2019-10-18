5acf2d9eae953be4fbac91aefa0516d3cb1a463b
Bence Szalai <bence-szalai@users.noreply.github.com>
2019-10-18 19:36:29 +0200

    overlap for non dorothea

--- code/run_viper.R
+++ code/run_viper.R
@@ -1,11 +1,11 @@
 library(viper)
 set.seed(19890904)
 
-for (dname in c('progeny','tcga')){
+for (dname in c('tcga')){
   if (dname=='progeny'){
     ncores=10
   } else {
-    ncores=4
+    ncores=1
   }
   for (abs_type in c('_abs','')){
     gex=read.csv(paste0('../results/benchmark/datasets/',dname,'_data.csv'),sep=',',header=T,row.names = 1)
@@ -13,6 +13,7 @@ for (dname in c('progeny','tcga')){
       gex=abs(gex)
     }
     genesets=list.files('../results/genesets/overlap/rdatas/')
+    genesets=genesets[-grep('dorothea',genesets)]
     genesets=unlist(lapply(genesets, function(x){substr(x,1,nchar(x)-6)}))
     for (geneset in genesets){
       load(paste0('../results/genesets/overlap/rdatas/',geneset,'.rdata'))
