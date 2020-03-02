43b13cc7ed0166921ba1a42d1347b5bbcc3f4593
Bence Szalai <bence-szalai@users.noreply.github.com>
2020-03-02 13:33:02 +0100

    overlap analysis runing

--- code/benchmark_gdsc_genomic_overlap.py
+++ code/benchmark_gdsc_genomic_overlap.py
@@ -10,10 +10,10 @@ def my_rocauc(x):
 gdsc=pd.read_csv('../data/gdsc/PANCAN_simple_MOBEM.rdata.tsv',
                 sep='\t',header=0,index_col=0)
 gdsc.columns=gdsc.columns.astype(int)
-scores=pd.read_csv('../results/benchmark/scores/gdsc/single/BEST_dorothea_AB_filtered.csv',
+scores=pd.read_csv('../results/benchmark/scores/gdsc/overlap/BEST_dorothea_AB_filtered.csv',
                 sep=',',header=0,index_col=0).T
 scores.index=pd.Series(scores.index).apply(lambda x:x[1:]).astype(int).values
-methods=[x[:-4] for x in os.listdir('../results/benchmark/scores/gdsc/single/')]
+methods=[x[:-4] for x in os.listdir('../results/benchmark/scores/gdsc/overlap/')]
 cosmics=list(set(gdsc.columns)&set(scores.index))
 gdsc=gdsc[cosmics]
 features=gdsc.index[gdsc.sum(1)>10]
@@ -28,7 +28,7 @@ except:
                                                          
 for method in methods:
     print(method)
-    scores=pd.read_csv('../results/benchmark/scores/gdsc/single/%s.csv' % method,
+    scores=pd.read_csv('../results/benchmark/scores/gdsc/overlap/%s.csv' % method,
                     sep=',',header=0,index_col=0).T
     scores.index=pd.Series(scores.index).apply(lambda x:x[1:]).astype(int).values
     scores=scores.loc[gdsc.columns]
@@ -38,15 +38,5 @@ for method in methods:
         print(pathway)
         y_tr=gdsc.loc[pathway]
         results.loc[pathway,:]=scores.apply(my_rocauc,0)
-    results.to_csv('../results/benchmark/rocaucs/gdsc/single/%s.csv' % method,
-                    sep=',')
-    
-results=pd.DataFrame(index=gdsc.index,columns=range(1000))
-for i in range(1000):
-    print(i)
-    y_pr=np.random.uniform(size=gdsc.shape[1])
-    for pathway in results.index:
-        y_tr=gdsc.loc[pathway]
-        results.loc[pathway,i]=ROCAUC(y_tr,y_pr)
-results.to_csv('../results/benchmark/rocaucs/gdsc/random_dist.csv',
+    results.to_csv('../results/benchmark/rocaucs/gdsc/overlap/%s.csv' % method,
                     sep=',')
