b16ac0411b660bbc27ec87a4aad73a5744b89eff
Bence Szalai <bence-szalai@users.noreply.github.com>
2020-06-09 17:43:17 +0200

    run viper

--- code/run_viper.R
+++ code/run_viper.R
@@ -9,14 +9,15 @@ for (dname in c('gdsc','progeny')){
     if (abs_type=='_abs'){
       gex=abs(gex)
     }
-    genesets=list.files('../results/genesets/single/rdatas/')
-     #genesets=genesets[-grep('dorothea',genesets)]
-    genesets=unlist(lapply(genesets, function(x){substr(x,1,nchar(x)-6)}))
-    for (geneset in genesets){
-      load(paste0('../results/genesets/single/rdatas/',geneset,'.rdata'))
-      activities = viper(eset = gex, regulon = viper_geneset, nes = T, 
-                         method = 'none' ,minsize = 4, eset.filter = F,cores = ncores)
-      write.csv(activities,paste0('../results/benchmark/scores/',dname,'/single/',geneset,abs_type,'.csv'))
+    for (sin_or_over in c('single','overlap')){
+      genesets=list.files(paste0('../results/genesets/',sin_or_over,'/rdatas/'))
+      genesets=unlist(lapply(genesets, function(x){substr(x,1,nchar(x)-6)}))
+      for (geneset in genesets){
+        load(paste0('../results/genesets/',sin_or_over,'/rdatas/',geneset,'.rdata'))
+        activities = viper(eset = gex, regulon = viper_geneset, nes = T, 
+                           method = 'none' ,minsize = 4, eset.filter = F,cores = ncores)
+        write.csv(activities,paste0('../results/benchmark/scores/',dname,'/',sin_or_over,'/',geneset,abs_type,'.csv'))
+      }
     }
   }
 }
